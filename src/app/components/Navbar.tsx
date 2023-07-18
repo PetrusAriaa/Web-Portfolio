a7064d6e76ff2b89e5b7750b34e036dd9dcd7dfc
PetrusAriaa <petrusariacr25@gmail.com>
2023-07-18 14:32:58 +0700

    style: add button element into navbar links

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link"
 
 export const Navbar = () => {
-    return (
-        <nav className="text-myWhite top-0 z-50 sticky bg-myBlack/50 backdrop-blur-sm border-b border-b-myWhite/25">
-            <ul className="hidden md:flex flex-row justify-center items-center py-4 px-78 space-x-24 text-lg font-medium">
-                <li><Link href={'/'}>Home</Link></li>
-                <li><Link href={'#about'} >About</Link></li>
-                <li><Link href={'/projects'} >Projects</Link></li>
-                <li><Link href={'/blog'} >Blog</Link></li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className="text-myWhite top-0 z-50 sticky bg-myBlack/50 backdrop-blur-sm border-b border-b-myWhite/25">
+      <ul className="hidden md:flex flex-row justify-center items-center py-4 px-78 space-x-24 text-lg font-medium">
+        <li><Link href={'/'}><button className="disabled:cursor-not-allowed" >About</button></Link></li>
+        <li><Link href={'/'}><button className="disabled:cursor-not-allowed" >Projects</button></Link></li>
+        <li><Link href={'/'}><button className="disabled:cursor-not-allowed" >Contact</button></Link></li>
+        <li><Link href={'/'}><button className="disabled:cursor-not-allowed" disabled>Blog</button></Link></li>
+      </ul>
+    </nav>
+  )
 }
