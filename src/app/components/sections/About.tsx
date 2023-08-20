0b64e33df58e5d813494487a301a682173cceb20
PetrusAriaa <petrusariacr25@gmail.com>
2023-08-21 00:20:33 +0700

    enhance responsiveness

--- src/app/components/sections/About.tsx
+++ src/app/components/sections/About.tsx
@@ -7,94 +7,91 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 export const About = () => {
-  const [currCategory, setCurrCategory] = useState(category.Languages)
   const [swiper, setSwiper] = useState<any>(null)
   const [activeSwiper, setActiveSwiper] = useState(0)
   return (
     <section
       id="about"
-      className="relative z-[20] bg-myBlack flex flex-col items-center justify-center text-myWhite font-semibold min-h-screen"
-    >
-      <div className="w-4/5 flex flex-col gap-4 ">
-        <div className="flex flex-col gap-4 ">
-          <h1 className="text-myBlue1 text-5xl">Backstory</h1>
-          <h1 className="text-myWhite font-normal text-lg">
+      className="relative py-20 -mt-2 md:py-10 lg:py-0 lg:mt-0 z-[20] bg-myBlack flex flex-col items-center justify-center min-h-screen">
+      <div className="w-4/5 flex flex-col gap-5 lg:gap-4 ">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-myBlue1 text-3xl font-semibold md:text-5xl">Backstory</h1>
+          <h1 className="text-myWhite font-normal text-sm md:text-base lg:text-lg">
             Petrus Aria is an Information Engineering student from Indonesia. He
             started his journey on Web Development from college. He has an
             interest on Cloud Computing. Watch out! He can turn into avocado
             anytime.
           </h1>
         </div>
-        <div className="flex flex-row justify-center gap-10 ">
-          <div className="w-2/3 flex flex-col gap-10">
-            <div className="flex flex-row gap-8">
+        <div className="flex flex-col-reverse md:flex-row justify-center gap-10 lg:h-[60vh]">
+          {/* Swiper */}
+          <div className="w-full md:w-2/3 flex flex-col gap-6 lg:gap-10">
+            <div className="flex flex-row justify-center md:justify-start gap-4 lg:gap-8">
               <div onClick={()=>{
                 setActiveSwiper(0)
                 swiper.slideTo(0)
                 }}
-                className={`cursor-pointer transition-colors duration-200 font-medium py-1 flex flex-col gap-2 after:h-[2px] after:bg-myBlue1 after:transition-transform hover:text-myBlue1 `+(
-                  activeSwiper === 0 ? "after:scale-x-100 text-myBlue1" : "after:scale-x-0"
+                className={`cursor-pointer text-xs sm:text-sm select-none transition-colors duration-200 font-medium py-1 flex flex-col gap-2 after:h-[2px] after:bg-myBlue1 after:transition-transform hover:text-myBlue1 `+(
+                  activeSwiper === 0 ? "after:scale-x-100 text-myBlue1" : "after:scale-x-0 text-myWhite"
                 )}>
                   Languages</div>
               <div onClick={()=>{
                 setActiveSwiper(1)
                 swiper.slideTo(1)
                 }}
-                className={`cursor-pointer transition-colors duration-200 font-medium py-1 flex flex-col gap-2 after:h-[2px] after:bg-myBlue1 after:transition-transform hover:text-myBlue1 `+(
-                  activeSwiper === 1 ? "after:scale-x-100 text-myBlue1" : "after:scale-x-0"
+                className={`cursor-pointer text-xs sm:text-sm select-none transition-colors duration-200 font-medium py-1 flex flex-col gap-2 after:h-[2px] after:bg-myBlue1 after:transition-transform hover:text-myBlue1 `+(
+                  activeSwiper === 1 ? "after:scale-x-100 text-myBlue1" : "after:scale-x-0 text-myWhite"
                 )}>
                   Frontend</div>
               <div onClick={()=>{
                 setActiveSwiper(2)
                 swiper.slideTo(2)
                 }}
-                className={`cursor-pointer transition-colors duration-200 font-medium py-1 flex flex-col gap-2 after:h-[2px] after:bg-myBlue1 after:transition-transform hover:text-myBlue1 `+(
-                  activeSwiper === 2 ? "after:scale-x-100 text-myBlue1" : "after:scale-x-0"
+                className={`cursor-pointer text-xs sm:text-sm select-none transition-colors duration-200 font-medium py-1 flex flex-col gap-2 after:h-[2px] after:bg-myBlue1 after:transition-transform hover:text-myBlue1 `+(
+                  activeSwiper === 2 ? "after:scale-x-100 text-myBlue1" : "after:scale-x-0 text-myWhite"
                 )}>
                   Backend</div>
               <div onClick={()=>{
                 setActiveSwiper(3)
                 swiper.slideTo(3)
                 }}
-                className={`cursor-pointer transition-colors duration-200 font-medium py-1 flex flex-col gap-2 after:h-[2px] after:bg-myBlue1 after:transition-transform hover:text-myBlue1 `+(
-                  activeSwiper === 3 ? "after:scale-x-100 text-myBlue1" : "after:scale-x-0"
+                className={`cursor-pointer text-xs sm:text-sm select-none transition-colors duration-200 font-medium py-1 flex flex-col gap-2 after:h-[2px] after:bg-myBlue1 after:transition-transform hover:text-myBlue1 `+(
+                  activeSwiper === 3 ? "after:scale-x-100 text-myBlue1" : "after:scale-x-0 text-myWhite"
                 )}>
                   Tools</div>
             </div>
-            <div className="min-h-[20vw] bg-red-500/25">
+            <div className="h-full">
               <Swiper
                 slidesPerView={1}
                 spaceBetween={100}
-                className="w-[90%] !overflow-y-visible"
+                className="!overflow-y-visible h-full"
                 grabCursor
                 onSwiper={setSwiper}
                 onSlideChange={(swiper)=>setActiveSwiper(swiper.activeIndex)}
               >
                 <SwiperSlide>
-                  <div className=" bg-red-500/25">
-                    <div className="grid grid-cols-3 text-center gap-y-8">
-                      {category.Languages.map((item, index)=>{
-                        return <LanguageBadge key={index} color={item.color ? item.color : "bg-myWhite text-myBlack"} icon={item.icon}>{item.title}</LanguageBadge>
-                      })}
-                    </div>
+                  <div className="grid grid-cols-3 text-center gap-2 md:gap-y-4 md:gap-x-5 lg:gap-y-8 lg:gap-x-10">
+                    {category.Languages.map((item, index)=>{
+                      return <LanguageBadge key={index} color={item.color ? item.color : "bg-myWhite text-myBlack"} icon={item.icon}>{item.title}</LanguageBadge>
+                    })}
                   </div>
                 </SwiperSlide>
                 <SwiperSlide >
-                  <div className="grid grid-cols-3 text-center gap-y-8">
+                  <div className="grid grid-cols-3 text-center gap-2 md:gap-y-4 md:gap-x-5 lg:gap-y-8 lg:gap-x-10">
                     {category.Frontend.map((item, index)=>{
                       return <LanguageBadge key={index} color={item.color ? item.color : "bg-myWhite text-myBlack"} icon={item.icon}>{item.title}</LanguageBadge>
                     })}
                   </div>
                 </SwiperSlide>
                 <SwiperSlide >
-                  <div className="grid grid-cols-3 text-center gap-y-8">
+                  <div className="grid grid-cols-3 text-center gap-2 md:gap-y-4 md:gap-x-5 lg:gap-y-8 lg:gap-x-10">
                     {category.Backend.map((item, index)=>{
                       return <LanguageBadge key={index} color={item.color ? item.color : "bg-myWhite text-myBlack"} icon={item.icon}>{item.title}</LanguageBadge>
                     })}
                   </div>
                 </SwiperSlide>
                 <SwiperSlide >
-                  <div className="grid grid-cols-3 text-center gap-4">
+                  <div className="grid grid-cols-3 text-center gap-2 md:gap-y-4 md:gap-x-5 lg:gap-y-8 lg:gap-x-10">
                     {category.Tools.map((item, index)=>{
                       return <LanguageBadge key={index} color={item.color ? item.color : "bg-myWhite text-myBlack"} icon={item.icon}>{item.title}</LanguageBadge>
                     })}
@@ -103,8 +100,9 @@ export const About = () => {
               </Swiper>
             </div>
           </div>
-          <div className="w-3/5 flex justify-center">
-            <div className="h-96 w-72 bg-slate-300/30 animate-pulse"></div>
+          {/* Image */}
+          <div className="flex justify-center w-full">
+            <div className="h-48 lg:h-96 w-full bg-slate-300/30 animate-pulse"></div>
           </div>
         </div>
       </div>
--- src/app/components/ui/LanguageBadge.tsx
+++ src/app/components/ui/LanguageBadge.tsx
@@ -9,10 +9,10 @@ interface LanguageBadgeProps extends HTMLAttributes<LanguageBadgeProps> {
 
 export const LanguageBadge: FC <LanguageBadgeProps> = ({icon, children, color}) => {
     return(
-        <div className="flex flex-row justify-center items-center ">
-            <div className={`${color} cursor-pointer flex flex-row justify-center items-center gap-2 font-normal py-1 min-w-fit w-4/5 rounded-md`}>
-                <div>{icon}</div>
-                <h1>{children}</h1>
+        <div className="flex flex-row justify-center items-center">
+            <div className={`${color} cursor-pointer flex flex-row justify-center items-center gap-2 py-1 min-w-fit w-full rounded-md`}>
+                <div className="hidden sm:block">{icon}</div>
+                <h1 className="select-none font-medium text-xs lg:text-base">{children}</h1>
             </div>
         </div>
     )
