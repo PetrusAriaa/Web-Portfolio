cff2dc95899b7b130c3f0c33967e83af6ef92171
PetrusAriaa <petrusariacr25@gmail.com>
2023-08-21 00:51:22 +0700

    enhance responsiveness

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,42 +1,66 @@
-import { SiGithub, SiInstagram, SiLinkedin } from "react-icons/si";
-import { TbMail } from "react-icons/tb";
-import { GoLocation, GoOrganization } from "react-icons/go";
+import {SiGithub, SiInstagram, SiLinkedin} from "react-icons/si";
+import {TbMail} from "react-icons/tb";
+import {GoLocation, GoOrganization} from "react-icons/go";
 import Link from "next/link";
 
 export const Footer = () => {
-    return (
-        <footer id="footer" className="flex flex-col justify-center pt-36 px-64 bg-myBlack2">
-            <div className="space-y-4">
-                <h1 className="text-myBlue1 font-semibold text-5xl">Petrus Aria</h1>
-                <h1 className="text-myWhite font-semibold text-5xl">Website Developer</h1>
+  return (
+    <footer
+      id="footer"
+      className="pt-16 w-full flex flex-col justify-center items-center bg-myBlack2"
+    >
+      <div className="w-4/5 flex flex-col gap-8">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-myBlue1 font-bold text-5xl md:text-7xl">
+            Petrus Aria
+          </h1>
+          <h1 className="text-myWhite font-semibold text-3xl md:text-5xl">
+            Website Developer
+          </h1>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-row gap-10">
+            <div className="flex flex-row gap-2 items-center">
+              <GoLocation color="#6BE7F1" />
+              <p className="text-myWhite text-xs sm:text-base">Indonesia</p>
             </div>
-            <div className="flex flex-row space-x-10 mt-20">
-                <div className="flex flex-row space-x-2 items-center">
-                    <GoLocation color="#6BE7F1"/>
-                    <p className="text-myWhite">Indonesia</p>
-                </div>
-                <div className="flex flex-row space-x-2 items-center">
-                    <GoOrganization color="#6BE7F1"/>
-                    <p className="text-myWhite">Universitas Gadjah Mada</p>
-                </div>
+            <div className="flex flex-row gap-2 items-center">
+              <GoOrganization color="#6BE7F1" />
+              <p className="text-myWhite text-xs sm:text-base">
+                Universitas Gadjah Mada
+              </p>
             </div>
-            <div className="flex flex-row space-x-2 items-center">
-                <TbMail color="#6BE7F1"/>
-                <p className="text-myWhite">petrusariacr25@gmail.com</p>
-            </div>
-            <section id="Socials" className="py-4 mt-4">
-                <div className="flex flex-row justify-center items-center gap-10">
-                <Link href={'#'} className="p-3 rounded-full transition-colors duration-200 hover:bg-slate-800 active:scale-95 focus:outline focus:outline-2 focus:outline-myBlue1 focus:outline-offset-2">
-                    <SiGithub color="#6BE7F1" size={25}/>
-                </Link>
-                <Link href={'#'} className="p-3 rounded-full transition-colors duration-200 hover:bg-slate-800 active:scale-95 focus:outline focus:outline-2 focus:outline-myBlue1 focus:outline-offset-2">
-                    <SiLinkedin color="#6BE7F1" size={25}/>
-                </Link>
-                <Link href={'#'} className="p-3 rounded-full transition-colors duration-200 hover:bg-slate-800 active:scale-95 focus:outline focus:outline-2 focus:outline-myBlue1 focus:outline-offset-2">
-                    <SiInstagram color="#6BE7F1" size={25}/>
-                </Link>
-                </div>
-            </section>
-        </footer>
-    )
-}
+          </div>
+          <div className="flex flex-row gap-2 items-center">
+            <TbMail color="#6BE7F1" />
+            <p className="text-myWhite text-xs sm:text-base">
+              petrusariacr25@gmail.com
+            </p>
+          </div>
+        </div>
+      </div>
+      <section id="Socials" className="py-4 mt-4">
+        <div className="flex flex-row justify-center items-center gap-10">
+          <Link
+            href={"#"}
+            className="p-3 rounded-full transition-colors duration-200 hover:bg-slate-800 active:scale-95 focus:outline focus:outline-2 focus:outline-myBlue1 focus:outline-offset-2"
+          >
+            <SiGithub color="#6BE7F1" size={25} />
+          </Link>
+          <Link
+            href={"#"}
+            className="p-3 rounded-full transition-colors duration-200 hover:bg-slate-800 active:scale-95 focus:outline focus:outline-2 focus:outline-myBlue1 focus:outline-offset-2"
+          >
+            <SiLinkedin color="#6BE7F1" size={25} />
+          </Link>
+          <Link
+            href={"#"}
+            className="p-3 rounded-full transition-colors duration-200 hover:bg-slate-800 active:scale-95 focus:outline focus:outline-2 focus:outline-myBlue1 focus:outline-offset-2"
+          >
+            <SiInstagram color="#6BE7F1" size={25} />
+          </Link>
+        </div>
+      </section>
+    </footer>
+  );
+};
--- src/app/components/sections/Projects.tsx
+++ src/app/components/sections/Projects.tsx
@@ -1,13 +1,33 @@
-export const  Projects = () => {
+import Link from "next/link";
+
+export const Projects = () => {
   return (
-    <section id="projects" className="w-full min-h-screen flex flex-col justify-center text-myWhite font-semibold bg-amber-500/25">
-      <div className="space-y-4">
-        <h1 className="text-myBlue1 text-5xl">Recent Works</h1>
-        <h1 className="text-myWhite font-normal text-lg">{"As a blacksmith, these are his projects"}</h1>
-      </div>
-      <div className="flex flex-row justify-end">
-        <button className="bg-myBlue1 px-6 py-1 rounded-lg">More</button>
+    <section
+      id="projects"
+      className="w-full min-h-screen flex flex-col items-center justify-center"
+    >
+      <div className="w-4/5 flex flex-col gap-4">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-myBlue1 text-3xl font-semibold md:text-5xl">
+            Recent Works
+          </h1>
+          <h1 className="text-myWhite font-normal text-sm md:text-base lg:text-lg">
+            As a blacksmith, these are his projects
+          </h1>
+        </div>
+        <div className="flex flex-row justify-end">
+          <Link href={"/"}>
+            <button className="bg-myBlue1 px-5 py-1 rounded-md text-sm font-medium select-none">
+              More
+            </button>
+          </Link>
+        </div>
+        <div className="flex flex-col md:flex-row gap-4">
+          <div className="w-full aspect-video bg-slate-500/50 animate-pulse"></div>
+          <div className="w-full aspect-video bg-slate-500/50 animate-pulse"></div>
+          <div className="w-full aspect-video bg-slate-500/50 animate-pulse"></div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
