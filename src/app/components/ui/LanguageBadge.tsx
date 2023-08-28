4a66970c924d461c0676b4691fda8fd58a67b086
PetrusAriaa <petrusariacr25@gmail.com>
2023-08-28 15:11:53 +0700

    style: fix responsive spacing

--- src/app/components/ui/LanguageBadge.tsx
+++ src/app/components/ui/LanguageBadge.tsx
@@ -1,19 +1,26 @@
-import { FC, HTMLAttributes } from "react"
-
+import {FC, HTMLAttributes} from "react";
 
 interface LanguageBadgeProps extends HTMLAttributes<LanguageBadgeProps> {
-    icon: any,
-    children: React.ReactNode,
-    color: string
+  icon: any;
+  children: React.ReactNode;
+  color: string;
 }
 
-export const LanguageBadge: FC <LanguageBadgeProps> = ({icon, children, color}) => {
-    return(
-        <div className="flex flex-row justify-center items-center">
-            <div className={`${color} cursor-pointer flex flex-row justify-center items-center gap-2 py-1 min-w-fit w-full rounded-md`}>
-                <div className="hidden sm:block">{icon}</div>
-                <h1 className="select-none font-medium text-xs lg:text-base">{children}</h1>
-            </div>
-        </div>
-    )
-}
+export const LanguageBadge: FC<LanguageBadgeProps> = ({
+  icon,
+  children,
+  color,
+}) => {
+  return (
+    <div className="flex flex-row justify-center items-center">
+      <div
+        className={`${color} cursor-pointer flex flex-row justify-center items-center gap-2 py-1 min-w-fit w-4/5 md:w-full rounded-md`}
+      >
+        <div>{icon}</div>
+        <h1 className="select-none font-medium text-xs lg:text-base">
+          {children}
+        </h1>
+      </div>
+    </div>
+  );
+};
