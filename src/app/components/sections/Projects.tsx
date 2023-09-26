e63e3d835ea602c790d8cdc7f6f7fcb1e1309bfa
PetrusAriaa <petrusariacr25@gmail.com>
2023-09-26 21:57:27 +0700

    feat: add new project

--- src/app/components/sections/Projects.tsx
+++ src/app/components/sections/Projects.tsx
@@ -1,21 +1,8 @@
 import Link from "next/link";
 import {ProjectCard} from "../ui/ProjectCard";
-
 import PocketBase from "pocketbase";
-import {useEffect} from "react";
 
 export const Projects = () => {
-  const pb = new PocketBase("https://mashed-potato.pockethost.io");
-
-  const getProjectsData = async () => {
-    const res = await pb.collection("projects").getFullList();
-
-    console.log(res);
-  };
-
-  useEffect(() => {
-    getProjectsData();
-  }, []);
   return (
     <section
       id="projects"
@@ -31,7 +18,7 @@ export const Projects = () => {
           </h1>
         </div>
         <div className="flex flex-row justify-end">
-          <Link href={"/"}>
+          <Link href={"/projects"}>
             <button className="bg-myBlue1 px-5 py-1 rounded-md text-sm font-medium select-none">
               More
             </button>
@@ -43,18 +30,14 @@ export const Projects = () => {
             title="PPSMB Kesatria 2023"
             desc="PPSMB Kesatria adventure – a project where I proudly put on my web developer cape! Picture this: a journey filled with lines of code, pixels coming to life, and a sprinkle of tech wizardry. As the web developer of 'PPSMB Kesatria,' I was like a modern-day digital bard, crafting an interactive symphony for users to dance through."
             stack={["aaa"]}
+            projectLink="https://ppsmb-ft.vercel.app/"
           />
           <ProjectCard
-            img="/projects/Screenshot from 2023-08-28 20-54-10.png"
-            title="PPSMB Kesatria 2023"
-            desc="PPSMB Kesatria adventure – a project where I proudly put on my web developer cape! Picture this: a journey filled with lines of code, pixels coming to life, and a sprinkle of tech wizardry. As the web developer of 'PPSMB Kesatria,' I was like a modern-day digital bard, crafting an interactive symphony for users to dance through."
+            img="/projects/Screenshot 2023-10-02 190111.png"
+            title="Komunitas Profesi GKIPI"
+            desc="Web Application for Komunitas Profesi GKIPI"
             stack={["aaa"]}
-          />
-          <ProjectCard
-            img="/projects/Screenshot from 2023-08-28 20-54-10.png"
-            title="PPSMB Kesatria 2023"
-            desc="PPSMB Kesatria adventure – a project where I proudly put on my web developer cape! Picture this: a journey filled with lines of code, pixels coming to life, and a sprinkle of tech wizardry. As the web developer of 'PPSMB Kesatria,' I was like a modern-day digital bard, crafting an interactive symphony for users to dance through."
-            stack={["aaa"]}
+            projectLink="https://komunitasprofesigkipi.org/"
           />
         </div>
       </div>
