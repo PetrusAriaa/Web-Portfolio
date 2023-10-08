4a3916c0f8958cf97dadd213850f4a6dd61f7719
PetrusAriaa <petrusariacr25@gmail.com>
2023-10-08 22:49:46 +0700

    feat: add drawer for mobile navigation

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,30 +1,36 @@
 import Link from "next/link";
+import {MobileDrawer} from "./MobileDrawer";
 
 export const Navbar = () => {
   return (
-    <nav className="text-myWhite top-0 z-[999] sticky bg-myBlack/50 backdrop-blur-sm border-b border-b-myWhite/25">
-      <ul className="hidden md:flex flex-row justify-center items-center py-4 px-78 space-x-24 text-lg font-medium">
-        <li>
-          <Link href={"/"}>
-            <button className="hover:text-myBlue1">About</button>
-          </Link>
-        </li>
-        <li>
-          <Link href={"/projects"}>
-            <button className="hover:text-myBlue1">Projects</button>
-          </Link>
-        </li>
-        <li>
-          <Link href={"/knowme"}>
-            <button className="hover:text-myBlue1">Know Me</button>
-          </Link>
-        </li>
-        <li>
-          <Link href={"/blog"}>
-            <button className="hover:text-myBlue1">Blog</button>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="text-myWhite top-0 z-[999] sticky bg-transparent md:bg-myBlack/50 backdrop-blur-0 md:backdrop-blur-sm border-0 md:border-b md:border-b-myWhite/25">
+        <ul className="hidden md:flex flex-row justify-center items-center py-4 px-78 space-x-24 text-lg font-medium">
+          <li>
+            <Link href={"/"}>
+              <button className="hover:text-myBlue1">About</button>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/projects"}>
+              <button className="hover:text-myBlue1">Projects</button>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/knowme"}>
+              <button className="hover:text-myBlue1">Know Me</button>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/blog"}>
+              <button className="hover:text-myBlue1">Blog</button>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <nav className="bottom-10 right-5 fixed z-[999] md:hidden">
+        <MobileDrawer />
+      </nav>
+    </>
   );
 };
