7d7009f62f53de9e9aa32043bcafe9e6eac80a8f
PetrusAriaa <petrusariacr25@gmail.com>
2023-12-07 20:03:15 +0700

    feat: add page Login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-myBlack text-myWhite">
+      <h1>Login</h1>
+    </div>
+  );
+};
+
+export default Login;
