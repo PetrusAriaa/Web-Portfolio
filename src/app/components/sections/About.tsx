8956303a93e062eae0a80ef04fe42aac6e780112
PetrusAriaa <petrusariacr25@gmail.com>
2023-12-07 19:34:17 +0700

    fix: revamp About card

--- src/app/components/sections/About.tsx
+++ src/app/components/sections/About.tsx
@@ -14,199 +14,35 @@ export const About = () => {
   const [swiper, setSwiper] = useState<any>(null);
   const [activeSwiper, setActiveSwiper] = useState(0);
   return (
-    <section className="relative py-20 -mt-2 md:py-10 lg:py-0 lg:mt-0 z-[20] bg-myBlack2 flex flex-col items-center justify-center min-h-[50rem]">
-      <Card className="w-4/5 md:max-w-[75rem] flex flex-col gap-5 lg:gap-4 py-10 lg:px-10 lg:py-20 bg-myBlack">
-        <div className="flex flex-col gap-4">
-          <h1
-            id="about"
-            className="text-myBlue1 text-3xl font-semibold md:text-5xl"
-          >
-            Backstory
-          </h1>
-          <h1 className="text-myWhite font-normal text-sm md:text-base lg:text-lg">
-            Petrus Aria is an Information Engineering student from Indonesia. He
-            started his journey on Web Development from college. He has an
-            interest on Cloud Computing. Watch out! He can turn into avocado
-            anytime.
-          </h1>
-        </div>
-        <div className="flex flex-col-reverse md:flex-row justify-center gap-10 lg:h-fit">
-          {/* Swiper */}
-          <div className="w-full md:w-2/3 flex flex-col gap-6 lg:gap-10">
-            <div className="flex flex-row justify-center md:justify-start gap-4 lg:gap-8">
-              <div
-                onClick={() => {
-                  setActiveSwiper(0);
-                  swiper.slideTo(0);
-                }}
-                className={
-                  `cursor-pointer text-xs sm:text-sm select-none transition-colors duration-200 font-medium py-1 flex flex-col gap-2 after:h-[2px] after:bg-myBlue1 after:transition-transform hover:text-myBlue1 ` +
-                  (activeSwiper === 0
-                    ? "after:scale-x-100 text-myBlue1"
-                    : "after:scale-x-0 text-myWhite")
-                }
-              >
-                Languages
-              </div>
-              <div
-                onClick={() => {
-                  setActiveSwiper(1);
-                  swiper.slideTo(1);
-                }}
-                className={
-                  `cursor-pointer text-xs sm:text-sm select-none transition-colors duration-200 font-medium py-1 flex flex-col gap-2 after:h-[2px] after:bg-myBlue1 after:transition-transform hover:text-myBlue1 ` +
-                  (activeSwiper === 1
-                    ? "after:scale-x-100 text-myBlue1"
-                    : "after:scale-x-0 text-myWhite")
-                }
-              >
-                Frontend
-              </div>
-              <div
-                onClick={() => {
-                  setActiveSwiper(2);
-                  swiper.slideTo(2);
-                }}
-                className={
-                  `cursor-pointer text-xs sm:text-sm select-none transition-colors duration-200 font-medium py-1 flex flex-col gap-2 after:h-[2px] after:bg-myBlue1 after:transition-transform hover:text-myBlue1 ` +
-                  (activeSwiper === 2
-                    ? "after:scale-x-100 text-myBlue1"
-                    : "after:scale-x-0 text-myWhite")
-                }
-              >
-                Backend
-              </div>
-              <div
-                onClick={() => {
-                  setActiveSwiper(3);
-                  swiper.slideTo(3);
-                }}
-                className={
-                  `cursor-pointer text-xs sm:text-sm select-none transition-colors duration-200 font-medium py-1 flex flex-col gap-2 after:h-[2px] after:bg-myBlue1 after:transition-transform hover:text-myBlue1 ` +
-                  (activeSwiper === 3
-                    ? "after:scale-x-100 text-myBlue1"
-                    : "after:scale-x-0 text-myWhite")
-                }
-              >
-                Tools
-              </div>
-            </div>
-            <div className="h-full flex flex-row items-center ">
-              {/* <Swiper
-                slidesPerView={1}
-                spaceBetween={100}
-                className="!overflow-y-visible h-full"
-                grabCursor
-                onSwiper={setSwiper}
-                onSlideChange={(swiper) => setActiveSwiper(swiper.activeIndex)}
-              >
-                <SwiperSlide>
-                  <div className="grid grid-cols-2 md:grid-cols-3 text-center gap-2 md:gap-y-4 md:gap-x-5 lg:gap-y-8 lg:gap-x-10">
-                    {category.Languages.map((item, index) => {
-                      return (
-                        <LanguageBadge
-                          key={index}
-                          color={
-                            item.color ? item.color : "bg-myWhite text-myBlack"
-                          }
-                          icon={item.icon}
-                        >
-                          {item.title}
-                        </LanguageBadge>
-                      );
-                    })}
-                  </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <div className="grid grid-cols-2 md:grid-cols-3 text-center gap-2 md:gap-y-4 md:gap-x-5 lg:gap-y-8 lg:gap-x-10">
-                    {category.Frontend.map((item, index) => {
-                      return (
-                        <LanguageBadge
-                          key={index}
-                          color={
-                            item.color ? item.color : "bg-myWhite text-myBlack"
-                          }
-                          icon={item.icon}
-                        >
-                          {item.title}
-                        </LanguageBadge>
-                      );
-                    })}
-                  </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <div className="grid grid-cols-2 md:grid-cols-3 text-center gap-2 md:gap-y-4 md:gap-x-5 lg:gap-y-8 lg:gap-x-10">
-                    {category.Backend.map((item, index) => {
-                      return (
-                        <LanguageBadge
-                          key={index}
-                          color={
-                            item.color ? item.color : "bg-myWhite text-myBlack"
-                          }
-                          icon={item.icon}
-                        >
-                          {item.title}
-                        </LanguageBadge>
-                      );
-                    })}
-                  </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <div className="grid grid-cols-2 md:grid-cols-3 text-center gap-2 md:gap-y-4 md:gap-x-5 lg:gap-y-8 lg:gap-x-10">
-                    {category.Tools.map((item, index) => {
-                      return (
-                        <LanguageBadge
-                          key={index}
-                          color={
-                            item.color ? item.color : "bg-myWhite text-myBlack"
-                          }
-                          icon={item.icon}
-                        >
-                          {item.title}
-                        </LanguageBadge>
-                      );
-                    })}
-                  </div>
-                </SwiperSlide>
-              </Swiper> */}
-            </div>
-            <div className="hidden md:flex justify-around">
-              <button
-                className="transition-colors duration-200 hover:bg-slate-200/10 active:bg-slate-200/25 p-2 rounded-full flex justify-center items-center"
-                onClick={() => {
-                  if (activeSwiper === 0) {
-                    setActiveSwiper(3);
-                    swiper.slideTo(3);
-                  } else {
-                    setActiveSwiper(activeSwiper - 1);
-                    swiper.slideTo(activeSwiper - 1);
-                  }
-                }}
-              >
-                <FiChevronLeft size={30} className="text-myWhite" />
-              </button>
-              <button
-                className="transition-colors duration-200 hover:bg-slate-200/10 active:bg-slate-200/25 p-2 rounded-full flex justify-center items-center"
-                onClick={() => {
-                  if (activeSwiper === 3) {
-                    setActiveSwiper(0);
-                    swiper.slideTo(0);
-                  } else {
-                    setActiveSwiper(activeSwiper + 1);
-                    swiper.slideTo(activeSwiper + 1);
-                  }
-                }}
-              >
-                <FiChevronRight size={30} className="text-myWhite" />
-              </button>
-            </div>
+    <section className="relative py-20 -mt-2 md:py-10 lg:py-0 lg:mt-0 z-[20] px-0 md:px-10 bg-myBlack flex flex-col items-center justify-center md:min-h-[35rem] lg:min-h-[50rem]">
+      <div className="flex flex-col-reverse px-2 md:flex-row items-center justify-center w-full md:w-[90%]">
+        <Card className="relative z-10 w-full md:w-[90%] md:max-w-[70rem] flex flex-row gap-5 lg:gap-4 px-4 py-2 md:px-10 md:py-20 bg-transparent backdrop-blur-md bg-gradient-to-bl from-myBlack2/30 to-myBlue1/10 border-t-2 border-r-2 border-myBlue1/30 shadow-myBlue1/20 shadow-lg md:drop-shadow-lg">
+          <div className="flex flex-col gap-4 w-full md:w-4/5">
+            <h1
+              id="about"
+              className="text-myBlue1 text-3xl font-semibold md:text-5xl"
+            >
+              Backstory
+            </h1>
+            <h1 className="text-myWhite font-normal text-sm md:text-base lg:text-lg">
+              Petrus Aria is an Information Engineering student from Indonesia.
+              He started his journey on Web Development from college. He has an
+              interest on Cloud Computing. Watch out! He can turn into avocado
+              anytime.
+            </h1>
           </div>
-          {/* Image */}
-          <div className="flex justify-center w-full max-h-36 md:max-h-72">
-            <Image src={Me2} alt="" />
-          </div>
-        </div>
-      </Card>
+        </Card>
+        <Image
+          src={Me2}
+          alt=""
+          className="p-8 w-[18rem] md:w-[20rem] lg:w-[36rem] relative md:-ml-36 lg:-ml-64 z-[11]"
+        />
+      </div>
+      <div className="w-full absolute top-5">
+        <h1 className="w-4/5 md:w-full text-[7rem] font-semibold text-myBlue1/20 leading-[6rem]">
+          Backstory
+        </h1>
+      </div>
     </section>
   );
 };
