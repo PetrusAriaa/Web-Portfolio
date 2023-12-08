faf7ea1acecb86d5459ad1bfe27449c295c545da
PetrusAriaa <petrusariacr25@gmail.com>
2023-12-09 02:00:45 +0700

    refactor: add FC interface for props to reuse component

--- src/app/components/ui/SwiperContent.tsx
+++ src/app/components/ui/SwiperContent.tsx
@@ -1,13 +1,26 @@
-import {FC} from "react";
+import {FC, JSXElementConstructor} from "react";
+import {IconType} from "react-icons/lib";
+
+interface Item {
+  title: string;
+  icon: JSX.Element;
+  color: string;
+}
 
 interface SwiperContentProps {
-  category: string;
+  category: Item[];
 }
+
+export type Category = Item[];
 
 const SwiperContent: FC<SwiperContentProps> = ({category}) => {
   return (
     <div className="text-myWhite ">
-      <h1>{category}</h1>
+      <ul>
+        {category.map((item: Item, index: number) => (
+          <li key={index}>{item.title}</li>
+        ))}
+      </ul>
     </div>
   );
 };
