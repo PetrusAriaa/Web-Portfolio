10ba139ffc2e4a7ef2d258bfaf7ad23f4d8e8fa9
PetrusAriaa <petrusariacr25@gmail.com>
2023-12-09 00:34:54 +0700

    chore: adjust skillset model

--- src/app/components/sections/category.tsx
+++ src/app/components/sections/category.tsx
@@ -19,44 +19,64 @@ import {
 } from "react-icons/si";
 
 export const category = {
-  Languages: [
+  LanguagesTools: [
     {
       title: "Javascript",
-      icon: <SiJavascript size={20}/>,
-      color: "bg-[#EFD81D] text-myBlack"
+      icon: <SiJavascript size={20} />,
+      color: "bg-[#EFD81D] text-myBlack",
     },
     {
       title: "Python",
-      icon: <SiPython size={20}/>,
-      color: "bg-[#366C9C] text-myBlack"
+      icon: <SiPython size={20} />,
+      color: "bg-[#366C9C] text-myBlack",
     },
     {
       title: "Typescript",
-      icon: <SiTypescript size={20}/>,
-      color: "bg-[#2F74C0] text-myWhite"
+      icon: <SiTypescript size={20} />,
+      color: "bg-[#2F74C0] text-myWhite",
     },
     {
       title: "Go",
-      icon: <SiGo size={20}/>,
-      color: "bg-[#67D0DE] text-myBlack"
+      icon: <SiGo size={20} />,
+      color: "bg-[#67D0DE] text-myBlack",
+    },
+    {
+      title: "Git",
+      icon: <SiGit />,
+      color: "bg-[#E84D31] text-myBlack",
+    },
+    {
+      title: "Docker",
+      icon: <SiDocker />,
+      color: "bg-[#2391E6] text-myBlack",
+    },
+    {
+      title: "Postman",
+      icon: <SiPostman />,
+      color: "bg-[#F56933] text-myWhite",
+    },
+    {
+      title: "Figma",
+      icon: <SiFigma />,
+      color: "bg-[#9D56F7] text-myBlack",
     },
   ],
 
   Frontend: [
     {
       title: "ReactJs",
-      icon: <SiReact size={20}/>,
-      color: "bg-[#5ED3F3] text-myBlack"
+      icon: <SiReact size={20} />,
+      color: "bg-[#5ED3F3] text-myBlack",
     },
     {
       title: "NextJs",
-      icon: <SiNextdotjs size={20}/>,
-      color: "bg-myWhite text-myBlack"
+      icon: <SiNextdotjs size={20} />,
+      color: "bg-myWhite text-myBlack",
     },
     {
       title: "Tailwindcss",
-      icon: <SiTailwindcss size={20}/>,
-      color: "bg-[#47A6AE] text-myBlack"
+      icon: <SiTailwindcss size={20} />,
+      color: "bg-[#47A6AE] text-myBlack",
     },
   ],
 
@@ -64,55 +84,32 @@ export const category = {
     {
       title: "NodeJs",
       icon: <SiNodedotjs />,
-      color: "bg-[#6EA45F] text-myBlack"
+      color: "bg-[#6EA45F] text-myBlack",
     },
     {
       title: "Express",
       icon: <SiExpress />,
-      color: "bg-myWhite text-myBlack"
+      color: "bg-myWhite text-myBlack",
     },
     {
       title: "Flask",
       icon: <SiFlask />,
-      color: "bg-[#3BA9BF] text-myBlack"
+      color: "bg-[#3BA9BF] text-myBlack",
     },
     {
       title: "MySQL",
       icon: <SiMysql />,
-      color: "bg-[#005D88] text-myWhite"
+      color: "bg-[#005D88] text-myWhite",
     },
     {
       title: "PostgreSQL",
       icon: <SiPostgresql />,
-      color: "bg-[#31648C] text-myWhite"
+      color: "bg-[#31648C] text-myWhite",
     },
     {
       title: "SQLite",
       icon: <SiSqlite />,
-      color: "text-[#31648C] bg-myWhite"
+      color: "text-[#31648C] bg-myWhite",
     },
   ],
-
-  Tools:[
-    {
-      title: "Git",
-      icon: <SiGit />,
-      color: "bg-[#E84D31] text-myBlack"
-    },
-    {
-      title: "Docker",
-      icon: <SiDocker />,
-      color: "bg-[#2391E6] text-myBlack"
-    },
-    {
-      title: "Postman",
-      icon: <SiPostman />,
-      color: "bg-[#F56933] text-myWhite"
-    },
-    {
-      title: "Figma",
-      icon: <SiFigma />,
-      color: "bg-[#9D56F7] text-myBlack"
-    },
-  ]
-}
+};
