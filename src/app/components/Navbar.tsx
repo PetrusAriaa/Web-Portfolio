b4dea97361b6e03a65efac5fe6d19f9c98de19f0
PetrusAriaa <petrusariacr25@gmail.com>
2023-12-11 00:27:58 +0700

    style: create my hideOnScroll Navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,56 +1,18 @@
-import Link from "next/link";
+import {useMediaQuery} from "@chakra-ui/react";
 
-import {scroller} from "react-scroll";
 import {MobileDrawer} from "./MobileDrawer";
+import MediumNavbar from "./MediumNavbar";
 
-const handleScroll = (target: string) => {
-  scroller.scrollTo(target, {
-    duration: 1500,
-    delay: 100,
-    smooth: "easeInOut",
-    offset: -110,
-  });
-};
+export const MyNavbar = () => {
+  const [isMedium] = useMediaQuery("(min-width: 768px)");
 
-export const Navbar = () => {
-  return (
+  return !isMedium ? (
     <>
-      <nav className="text-myWhite top-0 z-[999] sticky bg-transparent md:bg-myBlack/50 backdrop-blur-0 md:backdrop-blur-sm border-0 md:border-b md:border-b-myWhite/25">
-        <ul className="hidden md:flex flex-row justify-center items-center py-4 px-78 space-x-24 text-lg font-medium">
-          <li>
-            <button
-              onClick={() => handleScroll("hero")}
-              className="hover:text-myBlue1"
-            >
-              Home
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={() => handleScroll("about")}
-              className="hover:text-myBlue1"
-            >
-              About
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={() => handleScroll("projects")}
-              className="hover:text-myBlue1"
-            >
-              Projects
-            </button>
-          </li>
-          <li>
-            <Link href={"/blog"}>
-              <button className="hover:text-myBlue1">Blog</button>
-            </Link>
-          </li>
-        </ul>
-      </nav>
       <nav className="bottom-10 right-5 fixed z-[999] md:hidden">
         <MobileDrawer />
       </nav>
     </>
+  ) : (
+    <MediumNavbar />
   );
 };
